47b9c11fb7d498c998567e1571d75dbc7be31c9f
JustAlittleWolf <jakobi5000@gmail.com>
2025-09-12 16:26:33 +0200

    feat: bump polar, minestom, shadow and kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
-    id("com.gradleup.shadow") version "9.0.0-rc1"
+    kotlin("jvm") version "2.2.20"
+    id("com.gradleup.shadow") version "9.1.0"
 }
 
 group = "me.wolfii"
-version = "1.1.0"
+version = "1.2.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("net.minestom:minestom:2025.07.11-1.21.7")
+    implementation("net.minestom:minestom:2025.08.29-1.21.8")
     implementation("org.slf4j:slf4j-nop:2.0.17")
 
-    implementation("dev.hollowcube:polar:1.14.6")
+    implementation("dev.hollowcube:polar:1.14.7")
 }
 
 kotlin {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,18 +6,26 @@ import net.minestom.server.MinecraftServer
 import java.io.File
 import java.nio.file.Path
 import kotlin.math.abs
+import kotlin.math.sqrt
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     MinecraftServer.init()
 
     if (args.isEmpty()) {
-        println("usage: java -jar PolarWorldImporter-VERSION.jar <world directory> <radius: Int = 20>")
+        println("usage: java -jar PolarWorldImporter-VERSION.jar <world directory> <radius: Int = 20> <shape: String = \"SQUARE\"")
         exitProcess(1)
     }
     val directory = Path.of(args[0])
     val radius = if (args.size > 1) args[1].toInt() else 20
-    val polarWorld = AnvilPolar.anvilToPolar(directory) { x, z -> abs(x) + abs(z) < radius }
+    val shape = if (args.size > 2) args[2] else "SQUARE"
+    val polarWorld = AnvilPolar.anvilToPolar(
+        directory, when (shape) {
+            "DIAMOND" -> { x, z -> abs(x) + abs(z) < radius }
+            "SQUARE" -> { x, z -> abs(x) < radius && abs(z) < radius }
+            "CIRCLE" -> { x, z -> sqrt((x * x + z * z).toDouble()) < radius }
+            else -> throw IllegalStateException("Unknown shape: $shape")
+        })
     val worldName = directory.fileName.toString()
     File("./$worldName.polar").writeBytes(PolarWriter.write(polarWorld))
 
